cd9671e591abdd22ff17ae31e9236208ae749bb8
ciupa.kamil <194928@edu.p.lodz.pl>
2017-04-29 15:52:02 +0200

    Recreating

---
+++ Zadanie 2/ObjectIndexor.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadanie2Indeksator
+{
+    class ObjectIndexor
+    {
+        
+        public int idxSize = 10;
+
+
+        private Object[] objs = new Object[10];
+        public Object this[int i]
+        {
+            get
+            {
+               try
+                {
+                    return objs[i];
+                }
+                catch (Exception ex)
+                {
+                    throw new ArgumentOutOfRangeException("Indexor out of bounds", ex);
+                }
+                
+            }
+
+
+            set
+            {
+                if(i > objs.Length - 1)
+                {
+                    ChangeSize(i);
+                    objs[i] = value;
+                }
+                else
+                {
+                    objs[i] = value;
+                }
+                
+            }
+        }
+
+        public void Add(Object value)
+        {
+            objs[objs.Length-1] = value;
+        }
+
+
+        public void ChangeSize(int size)
+        {
+            Object[] temporary = new Object[idxSize];
+            for (int i = 0; i < objs.Length; i++)
+                temporary[i] = objs[i];
+
+            objs = new Object[idxSize + size];
+
+            for (int i = 0; i < temporary.Length; i++)
+                objs[i] = temporary[i];
+
+           System.Console.WriteLine("Size is bigger now \n");
+        }
+
+
+    }
+}
---
+++ Zadanie 2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Zadanie2Indeksator
+{
+    static class Program
+    {
+        /// <summary>
+        /// Główny punkt wejścia dla aplikacji.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            //Application.Run(new Form1());
+
+
+            ObjectIndexor objectIndexor = new ObjectIndexor();
+            objectIndexor[0] = new Object();
+            //Object object2 = objectIndexor[10];
+            objectIndexor[12] = new Object();
+            Object object2 = objectIndexor[10];
+        }
+    }
+}
