4b37f354be0c1c7e960e721d1eaa621bce8106b4
ciupa.kamil <194928@edu.p.lodz.pl>
2017-05-08 15:10:18 +0200

    zadanie 3 - start state

---
+++ Zadanie 3/Zadanie 3/Zadanie 3/ObjectIndexor.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadanie_3
+{
+    class ObjectIndexor
+    {
+
+        public int idxSize;
+        private int[] objs;
+
+        public ObjectIndexor()
+        {
+            this.idxSize = 1;
+            this.objs = new int[idxSize];
+        }
+
+
+
+        public int this[int i]
+        {
+            get
+            {
+                try
+                {
+                    if (i > idxSize)
+                    {
+                        throw new ArgumentOutOfRangeException();
+                    }
+                    else
+                    {
+                        return objs[i];
+                    }
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine(" Tablica nie ma indeksu " + i + "\n");
+                    return 0;
+                }
+            }
+
+
+            set
+            {
+                if (i > idxSize)
+                {
+                    idxSize += (i - idxSize) + 1;
+                    Array.Resize<int>(ref objs, idxSize);
+                    objs[i] = value;
+                }
+                else
+                {
+                    objs[i] = value;
+                }
+
+            }
+        }
+
+        public void Add(int value)
+        {
+            Array.Resize<int>(ref objs, objs.Length * 2);
+            objs[idxSize] = value;
+            idxSize++;
+        }
+
+
+        public void Printing()
+        {
+            for (int i = 0; i < idxSize; i++)
+                Console.WriteLine(i + ") komorka przechowuje " + objs[i] + "\n");
+
+        }
+
+    }
+}
---
+++ Zadanie 3/Zadanie 3/Zadanie 3/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Zadanie_3
+{
+    static class Program
+    {
+        /// <summary>
+        /// Główny punkt wejścia dla aplikacji.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            //Application.Run(new Form1());
+
+
+            ObjectIndexor objectIndexor = new ObjectIndexor();
+
+            /*
+            objectIndexor[0] = 1;
+            objectIndexor.Printing();
+            */
+
+            objectIndexor[3] = 4;
+            objectIndexor.Printing();
+            objectIndexor.Add(3);
+            objectIndexor.Add(12);
+            objectIndexor[9] = 66;
+            objectIndexor.Add(23);
+            objectIndexor.Printing();
+        }
+    }
+}
